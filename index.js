a6eb344e80367f825f99b3fa9279dbfc8ae96536
SorinGFS <sorin.gfs@protonmail.com>
2022-08-26 10:10:11 +0300

    initial

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const CleanCSS = require('clean-css');
+/**
+ * @class CleanCssAfterCompilePlugin - This is a example of a webpack plugin using `class`. The most common and easy to understand syntax
+ */
+class CleanCssAfterCompilePlugin {
+    /**
+     * @description Creates an instance of CleanCssAfterCompilePlugin.
+     * @param {object} options - Adds options from your configuration into the plugin for additional functionality
+     *
+     * @memberOf CleanCssAfterCompilePlugin
+     */
+    constructor(options) {
+        this.cleanCssOptions = {};
+        Object.assign(this, options);
+    }
+    /**
+     * @name apply
+     * @description This method is invoked by the Compiler on "registration" and an instance of the Compiler is passed in as a parameter.
+     * @param {Compiler} compiler - This is the Compiler instance. Calling `compiler.plugin('some-event', (someState) => {})` allows one to "plug into"
+     * the compiler and listen to events and update state/add side effects or perform additional functionality through the compiler/compilation life cycle
+     *
+     * @memberOf CleanCssAfterCompilePlugin
+     */
+    apply(compiler) {
+        compiler.hooks.done.tap('CleanCssAfterCompilePlugin', (stats) => {
+            if (this.isProduction) {
+                if (fs.existsSync(path.resolve(this.outputDir))) {
+                    console.log('[CleanCssAfterCompilePlugin] minifying compilation css assets...');
+                    const assets = stats.compilation.getAssets();
+                    if (assets.length) {
+                        assets.forEach((asset) => {
+                            if (/\.css$/.test(asset.name)) {
+                                const assetPath = path.resolve(this.outputDir, asset.name);
+                                const output = new CleanCSS(this.cleanCssOptions).minify([assetPath]);
+                                if (!output.errors.length) {
+                                    fs.writeFile(assetPath, output.styles, 'utf8', function (err) {
+                                        if (err) return console.log(err);
+                                    });
+                                    console.log('[CleanCssAfterCompilePlugin] minifying compilation css assets done!');
+                                } else {
+                                    console.log(output.errors);
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    throw new ReferenceError(`[CleanCssAfterCompilePlugin] invalid outputDir: ${this.outputDir}`);
+                }
+            }
+        });
+    }
+}
+
+module.exports = CleanCssAfterCompilePlugin;
---
+++ readme.md
@@ -0,0 +1,63 @@
+﻿### Clean CSS After Compile Plugin
+
+For some reason `webpack.optimization` with [CssMinimizerPlugin.cleanCssMinify](https://webpack.js.org/plugins/css-minimizer-webpack-plugin/#function) does not work as expected. This plugin aims to solve the same thing but after compilation is done. The `css` assets are modified in place without altering the references already set by `webpack`.
+
+#### Installation
+
+```shell
+npm i -D webpack-clean-css-after-compile-plugin
+```
+
+#### Configuration
+
+The plugin takes a single argument as object having:
+
+```js
+{
+    isProduction: boolean, // if true will run minification, else not
+    outputDir: string, // must match the webpack output path
+    cleanCssOptions: object, // optional, default is empty object
+}
+```
+
+For detailed `cleanCssOptions` see [clean-css documentation](https://github.com/clean-css/clean-css#constructor-options).
+
+#### Usage
+
+The following example shows a minimal scenario
+```js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanCssAfterCompilePlugin = require('webpack-clean-css-after-compile-plugin');
+
+// see clean-css docs for options, the following is default and is not required
+const cleanCssOptions = {};
+
+const isProduction = process.env.NODE_ENV === 'production';
+const sourceDir = path.resolve(__dirname, 'src');
+const outputDir = path.resolve(__dirname, 'web');
+
+module.exports = {
+    mode: isProduction ? 'production' : 'development',
+    entry: { index: path.resolve(sourceDir, 'index.js') },
+    output: {
+        path: outputDir,
+        // other options
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                // options.publicPath is required for asset imports in CSS, such as url()
+                use: [{ loader: MiniCssExtractPlugin.loader, options: { publicPath: '' } }, 'css-loader' ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({/* options */}),
+        new CleanCssAfterCompilePlugin({ isProduction, outputDir, cleanCssOptions }),
+    ],
+    devtool: isProduction ? false : 'source-map',
+};
+```
